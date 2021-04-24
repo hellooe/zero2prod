efb53fec8d30706b927a8f8401cc0691262e24de
hellooe <hellooe@qq.com>
2021-04-24 16:55:22 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
 
-    let lst_addr = std::env::var("LST_ADDR").unwrap_or("127.0.0.1:8000".into());
+    let lst_addr = std::env::var("LST_ADDR").unwrap_or_else(|_| "127.0.0.1:8000".into());
     let db_url = std::env::var("DATABASE_URL").unwrap();
 
     run(&lst_addr, &db_url).await
